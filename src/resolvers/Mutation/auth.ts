03b04b02b550fa5ec4d2f255b9e6f9e69952e29c
faheemsidd15 <blazefs@gmail.com>
2018-07-09 21:12:38 -0400

    initial commit

---
+++ src/resolvers/Mutation/auth.ts
@@ -0,0 +1,53 @@
+import * as bcrypt from "bcryptjs"
+import * as jwt from "jsonwebtoken"
+import { Context, createToken, getUserId } from "../../utils"
+
+export const auth = {
+	async refreshToken(parent, { token }, ctx: Context, info) {
+		const userId = getUserId(ctx, token)
+		return createToken(userId)
+	},
+	async signup(parent, args, ctx: Context, info) {
+		const password = await bcrypt.hash(args.password, 10)
+		const user = await ctx.db.mutation.createUser({
+			data: { ...args, password }
+		})
+
+		return {
+			token: createToken(user.id),
+			user
+		}
+	},
+
+	async login(parent, { email, password }, ctx: Context, info) {
+		const user = await ctx.db.query.user({ where: { email } })
+		if (!user) {
+			// throw new Error(`No such user found for email: ${email}`)
+			return {
+				error: {
+					field: "email",
+					msg: "No user found"
+				}
+			}
+		}
+
+		const valid = await bcrypt.compare(password, user.password)
+		if (!valid) {
+			// throw new Error('Invalid password')
+
+			return {
+				error: {
+					field: "password",
+					msg: "Invalid password"
+				}
+			}
+		}
+
+		return {
+			payload: {
+				token: createToken(user.id),
+				user
+			}
+		}
+	}
+}
