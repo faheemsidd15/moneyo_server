8b2d965c9c0557ab0f5d34d74f95e41c10c7618c
faheemsidd15 <blazefs@gmail.com>
2018-10-07 22:52:34 -0400

    Auth

--- src/utils.ts
+++ src/utils.ts
@@ -6,24 +6,21 @@ export interface Context {
 	request: any
 }
 
-export function getUserId(ctx: Context, jwtToken) {
-	let token = ""
-	if (jwtToken) {
-		token = jwtToken
-	} else {
-		const Authorization = ctx.request.get("Authorization")
+export function getUserId(ctx: Context) {
+	const Authorization = ctx.request.get("Authorization")
+	const token = Authorization.replace("Bearer ", "")
 
-		token = Authorization.replace("Bearer ", "")
-	}
 	if (token) {
-		const { userId } = jwt.verify(token, process.env.APP_SECRET) as { userId: string }
+		const { userId } = jwt.verify(token, process.env.APP_SECRET) as {
+			userId: string
+		}
 		return userId
 	}
 
 	throw new AuthError()
 }
 
-export const createToken = (userId: String) => jwt.sign({ userId, expiresIn: "1d" }, process.env.APP_SECRET)
+export const createToken = (userId: String) => jwt.sign({ userId }, process.env.APP_SECRET, { expiresIn: "1d" })
 
 export class AuthError extends Error {
 	constructor() {
