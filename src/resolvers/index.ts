c8e9f92e25575e4959a3e31318cae012ade93014
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2018-10-09 16:12:01 -0400

    comment

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,13 +1,18 @@
-import { Query } from './Query'
-import { auth } from './Mutation/auth'
-import { post } from './Mutation/post'
-import { AuthPayload } from './AuthPayload'
+import { Query } from "./Query"
+import { auth } from "./Mutation/auth"
+import { post } from "./Mutation/post"
+import { AuthPayload } from "./AuthPayload"
 
 export default {
-  Query,
-  Mutation: {
-    ...auth,
-    ...post,
-  },
-  AuthPayload,
+	Query,
+	Mutation: {
+		...auth,
+		...post
+	},
+	// User: {
+	//   posts: (user, input, ctx) => {
+	//     // return ctx.models.UserPosts.where({ userId: user.id })
+	//   }
+	// },
+	AuthPayload
 }
