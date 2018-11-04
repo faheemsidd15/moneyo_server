50556a206c399bef169e9766fe54684c76125ecb
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2018-11-04 01:25:27 -0400

    delete income

--- src/resolvers/Mutation/incomes.ts
+++ src/resolvers/Mutation/incomes.ts
@@ -1,7 +1,7 @@
 import { getUserId, Context } from "../../utils"
 
 export const income = {
-	async createIncome(parent, { name, amount, type, payDate }, ctx: Context, info) {
+	async createIncome(parent, { input: { name, amount, type, payDate } }, ctx: Context, info) {
 		const userId = getUserId(ctx)
 		return ctx.db.mutation.createIncome(
 			{
@@ -17,5 +17,17 @@ export const income = {
 			},
 			info
 		)
+	},
+	async deleteIncome(parent, { id }, ctx: Context, info) {
+		const userId = getUserId(ctx)
+		const IncomeExists = await ctx.db.exists.Income({
+			id,
+			user: { id: userId }
+		})
+		if (!IncomeExists) {
+			throw new Error(`Income not found or you're not the user`)
+		}
+
+		return ctx.db.mutation.deleteIncome({ where: { id } })
 	}
 }
