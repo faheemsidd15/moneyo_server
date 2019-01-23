dd5e6a32aed62b00f001fefe6f50b435143f3030
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2019-01-23 12:41:11 -0500

    update income function

--- src/resolvers/Mutation/incomes.ts
+++ src/resolvers/Mutation/incomes.ts
@@ -1,8 +1,30 @@
 import { getUserId, Context } from "../../utils"
 
 export const income = {
+	async updateIncome(parent, { input: { id, ...data } }, ctx: Context, info) {
+		// check income id to match userID otherwise dont post
+		const userId = getUserId(ctx)
+		const income = await ctx.db.query.income({ where: { id } })
+		console.log(income)
+
+		if (userId !== income.user.id) {
+			throw new Error("Not Authorized")
+		}
+
+		return ctx.db.mutation.updateIncome(
+			{
+				where: {
+					id
+				},
+				data
+			},
+			info
+		)
+	},
+
 	async createIncome(parent, { input: { name, amount, type, payDate } }, ctx: Context, info) {
 		const userId = getUserId(ctx)
+		//
 		return ctx.db.mutation.createIncome(
 			{
 				data: {
